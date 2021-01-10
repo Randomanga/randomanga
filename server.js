c19e73f50bc9fb95b04a3312cfe85b20283c7900
Wiz1991 <wizoftime92@gmail.com>
2021-01-10 19:24:31 +0100

    removed static react serving from express

--- server.js
+++ server.js
@@ -3,34 +3,26 @@ const path = require('path');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const app = express();
-
-if (process.env.NODE_ENV !== 'production')
-    require('dotenv').config();
+require('dotenv').config();
 
 const jwt_strategy = require('./middleware/passport-strategy');
-const passport = require('passport')
+const passport = require('passport');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
-passport.use('jwt',jwt_strategy)
+passport.use('jwt', jwt_strategy);
 
-app.use(authRoutes);
+//routes, all routes must use /api prefix because of nginx routing
+
+app.use('/api',authRoutes);
 
 app.use(function (req, res, next) {
     res.sendStatus(404);
 });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
-const PORT = process.env.PORT || 5006;
+const PORT = process.env.PORT || 5000;
 
 const mongoose = require('mongoose');
 const { allowedNodeEnvironmentFlags } = require('process');
@@ -41,12 +33,12 @@ mongoose.connect(dbURI, {
     useUnifiedTopology: true,
     useCreateIndex: true,
 });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 
 db.on('connected', () => {
     app.listen(PORT, () => {
-        console.log('-> Connected to the database ');
-        console.log(`-> Server running on port ${PORT}`);
+        console.log('Server running');
     });
 });
