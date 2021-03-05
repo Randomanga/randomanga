0b46b4c2eb13859aa798b700e451eac739ddc127
Wiz1991 <wizoftime92@gmail.com>
2021-03-05 17:30:11 +0100

    manga card grid

--- client/src/pages/Random/Random.js
+++ client/src/pages/Random/Random.js
@@ -1,14 +1,58 @@
+import Pill from "../../components/Pill/Pill";
+
 export default function Random(props) {
-    console.log(props.match.params.id);
-    return (
-        <main className="mt-14">
-            
-
-            
-
-
-
-
-        </main>
-    );
+  console.log(props.match.params.id);
+  return (
+    <main className="mt-32 w-screen ">
+      <section className="px-2 container max-w-7xl grid grid-cols-1  md:grid-cols-2 xl:grid-cols-3 auto-cols-fr gap-x-2 gap-y-4 mx-auto">
+        <article className="bg-gray-800 shadow-2xl flex ">
+          <div className="w-32  md:w-36 lg:w-40">
+            <img src="https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/nx30101-N77ngeqRINUf.jpg" />
+          </div>
+          <div className="flex-1 max-h-full flex flex-col">
+            <div className="w-full h-full">
+              <div className="overflow-y-scroll scrollbar scrollbar-thin  scrollbar-thumb-gray-700 scrollbar-track-transparent  max-h-40 md:max-h-44 lg:max-h-52 px-3">
+                <h3 className="font-bold text-lg lg:text-xl py-1  text-gray-300">
+                  One piece
+                </h3>
+                <div className="line-clamp-none text-xs  text-gray-500 hover:text-gray-300 leading-4 tracking-tight ">
+                  Everybody at Seiyo Elementary thinks that stylish and super
+                  cool Amu has it all: But nobody knows the real Amu, a shy girl
+                  who wishes she had the courage to truly be herself. Changing
+                  Amu’s life is going to take more than wishes and dreams–it’s
+                  going to take a little magic! One morning, Amu finds a
+                  surprise in her bed: three strange little eggs. Each egg
+                  contains a Guardian Character, an angel-like being who can
+                  give her the power to be someone new. With the help of her
+                  Guardian Characters, Amu is about to discover that her true
+                  self is even more amazing than she ever dreamed.\ Everybody at
+                  Seiyo Elementary thinks that stylish and super cool Amu has it
+                  all: But nobody knows the real Amu, a shy girl who wishes she
+                  had the courage to truly be herself. Changing Amu’s life is
+                  going to take more than wishes and dreams–it’s going to take a
+                  little magic! One morning, Amu finds a surprise in her bed:
+                  three strange little eggs. Each egg contains a Guardian
+                  Character, an angel-like being who can give her the power to
+                  be someone new. With the help of her Guardian Characters, Amu
+                  is about to discover that her true self is even more amazing
+                  than she ever dreamed.
+                </div>
+              </div>
+            </div>
+            <div className="w-full h-7 flex items-center justify-between">
+              <div className="flex space-x-1 h-5 flex-wrap overflow-hidden pl-3">
+                <Pill text="Action" />
+                <Pill text="Action" />
+                <Pill text="Action" />
+                <Pill text="Action" />
+              </div>
+              <div className="w-5 h-5">
+                <button>X</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,69 +1,72 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-    purge: [
-        './src/**/*.{js,jsx,ts,tsx}',
-        './public/index.html',
-        './src/**/**/*.js',
-        './src/**/**/**/*.js',
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            zIndex: {
-                '-1': '-1',
-                '-2': '-2',
-                '-3': '-3',
-            },
-            spacing: {
-                100: '26rem',
-                105: '28rem',
-                110: '30rem',
-                115: '35rem',
-            },
-            colors: {
-                darkGray: {
-                    50: '#f8f8f8',
-                    100: '#dbe1e8',
-                    200: '#b2becd',
-                    300: '#6c7983',
-                    400: '#454e56',
-                    500: '#2a2a35',
-                    600: '#12181b',
-                    
-                },
-                orange: {
-                    100: '#FFC966',
-                    200: '#FFC04D',
-                    300: '#FFB733',
-                    400: '#FFAE19',
-                    500: '#ffa500',
-                    600: '#E69400',
-                    700: '#CC8400',
-                    800: '#B37300',
-                    900: '#996300',
-                },
-            },
-            width: {
-                38: '15rem',
-            },
-            transitionProperty: {
-                height: 'height',
-            },
-            lineClamp: {
-                8: '8',
-                9: '9',
-            },
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/**/**/*.js",
+    "./src/**/**/**/*.js",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        "-1": "-1",
+        "-2": "-2",
+        "-3": "-3",
+      },
+      spacing: {
+        100: "26rem",
+        105: "28rem",
+        110: "30rem",
+        115: "35rem",
+      },
+      colors: {
+        darkGray: {
+          50: "#f8f8f8",
+          100: "#dbe1e8",
+          200: "#b2becd",
+          300: "#6c7983",
+          400: "#454e56",
+          500: "#2a2a35",
+          600: "#12181b",
         },
-    },
-    variants: {
-        extend: {
-            pointerEvents: ['hover'],
-            opacity: ['disabled'],
+        orange: {
+          100: "#FFC966",
+          200: "#FFC04D",
+          300: "#FFB733",
+          400: "#FFAE19",
+          500: "#ffa500",
+          600: "#E69400",
+          700: "#CC8400",
+          800: "#B37300",
+          900: "#996300",
         },
+      },
+      width: {
+        38: "15rem",
+      },
+      maxHeight: {
+        34: "136px",
+      },
+      transitionProperty: {
+        height: "height",
+      },
+      lineClamp: {
+        8: "8",
+        9: "9",
+      },
     },
-    plugins: [
-        require('@tailwindcss/line-clamp'),
-        require('tailwindcss-textshadow'),
-    ],
+  },
+  variants: {
+    extend: {
+      pointerEvents: ["hover"],
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("tailwindcss-textshadow"),
+    require("tailwind-scrollbar"),
+  ],
 };
