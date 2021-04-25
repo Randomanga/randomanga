8f76f3f2ec7a97a352b269bd42f0868e37d9e4a1
Wiz1991 <wizoftime92@gmail.com>
2021-04-25 13:44:40 +0200

    stash

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,51 +1,53 @@
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 
 export const getStoredUserAuth = () => {
-    const auth = JSON.parse(window.localStorage.getItem('rmgid'));
-    if (auth) {
-        const decoded = jwtDecode(auth.token);
-        if (decoded.exp * 1000 < Date.now()) {
-            window.localStorage.removeItem('rmgid');
-        } else {
-            return auth;
-        }
+  const auth = JSON.parse(window.localStorage.getItem("rmgid"));
+  if (auth) {
+    const decoded = jwtDecode(auth.token);
+    if (decoded.exp * 1000 < Date.now()) {
+      window.localStorage.removeItem("rmgid");
+    } else {
+      return auth;
     }
-    return null;
+  }
+  return null;
 };
 const authFetch = async (url, token) => {
-    try {
-        const res = await axios.get(url, {
-            headers: { Authorization: `Bearer ${token}` },
-        });
-        return res.data;
-    } catch (err) {
-        throw err;
-    }
+  try {
+    const res = await axios.get(url, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
 };
 const fetch = async (url) => {
-    try {
-        const res = await axios.get(url);
-        return res.data;
-    } catch (err) {
-        throw err;
-    }
+  try {
+    const res = await axios.get(url);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const fetcher = async (url, token) => {
-    try {
-        if (token) {
-            const data = await authFetch(url, token);
-            return data;
-        } else {
-            const data = await fetch(url);
-            return data;
-        }
-    } catch (err) {
-        const errorResponse = err.response;
-        const error = new Error('Error while fetching data.');
-        error.info = errorResponse.data.errors.message;
-        error.status = err.response.status;
-        throw error;
+  try {
+    if (token) {
+      const data = await authFetch(url, token);
+      console.log(data);
+      return data.data;
+    } else {
+      const data = await fetch(url);
+      console.log(data);
+      return data.data;
     }
+  } catch (err) {
+    const errorResponse = err.response;
+    const error = new Error("Error while fetching data.");
+    error.info = errorResponse.data.errors.message;
+    error.status = err.response.status;
+    throw error;
+  }
 };
--- client/src/utils/hooks/useDaily.js
+++ client/src/utils/hooks/useDaily.js
@@ -1,35 +1,28 @@
-import { useContext } from 'react';
-import useSWR from 'swr';
-import { AuthContext } from '../../context/AuthContext';
-import { fetcher } from '../helpers';
-
-
-
-
+import { useContext } from "react";
+import useSWR from "swr";
+import { AuthContext } from "../../context/AuthContext";
+import { fetcher } from "../helpers";
 
 export default function useDaily() {
-    const { user } = useContext(AuthContext);
-    const { data, error, mutate } = useSWR(
-        user
-            ? ['http://192.168.1.242:5000/api/manga/daily', user.token]
-            : 'http://192.168.1.242:5000/api/manga/daily',
-        fetcher,
-        {
-            onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
-                if (error.status === 404) return;
-                if (retryCount >= 5) return;
-                setTimeout(
-                    () => revalidate({ retryCount: retryCount + 1 }),
-                    5000
-                );
-            },
-            shouldRetryOnError: true,
-        }
-    );
-    return {
-        data,
-        isLoading: !error && !data,
-        error: error,
-        mutate: mutate,
-    };
+  const { user } = useContext(AuthContext);
+  const { data, error, mutate } = useSWR(
+    user
+      ? ["http://192.168.1.242:5000/api/manga/daily", user.token]
+      : "http://192.168.1.242:5000/api/manga/daily",
+    fetcher,
+    {
+      onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        if (error.status === 404) return;
+        if (retryCount >= 5) return;
+        setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 5000);
+      },
+      shouldRetryOnError: true,
+    }
+  );
+  return {
+    data,
+    isLoading: !error && !data,
+    error: error,
+    mutate: mutate,
+  };
 }
