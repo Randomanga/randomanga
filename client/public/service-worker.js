b9b3fe02dfe4578ae5a6b81ae1e1ae7292a77a59
Wiz1991 <wizoftime92@gmail.com>
2021-08-21 16:47:28 +0200

    service worker v update

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,15 +1,17 @@
-const CACHE = 'V1.0.1'
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+const CACHE = 'V1.0.2';
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js'
+);
 
 workbox.routing.registerRoute(
-    new RegExp('/*'),
-    new workbox.strategies.CacheFirst({
-        cacheName: CACHE
-    })
+  new RegExp('/*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: CACHE,
+  })
 );
 
 self.addEventListener('message', (event) => {
-    console.log('skip')
+  console.log('skip');
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
@@ -19,14 +21,16 @@ self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
   //Remove previous cached data from disk.
   evt.waitUntil(
-      caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-              if (key !== CACHE) {
-                  console.log('[ServiceWorker] Removing old cache', key);
-                  return caches.delete(key);
-                }
-          }));
-      })
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
