60ec9a480c057e37882e609fe1fd58af89c123f6
Wiz1991 <wizoftime92@gmail.com>
2021-09-01 15:56:52 +0200

    og

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -11,8 +11,9 @@ export const Home = (props) => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Randomanga - Randomanga</title>
+        <meta property="og:title" content="Randomanga - Randomanga" />
         <meta name="keywords" content="random manga, random manga generator, random list generator, manga recommendations, list recommendations, manga list, random manga list, manga, manga to read, what manga to read" />
-        <meta name="description" content="Need a random manga? Or need some recommendations? Use this website to get a random list of manga, browse user curated lists or find recommendations created by our recommendations engine. " />
+        <meta property="og:description" content="Need a random manga? Or need some recommendations? Use this website to get a random list of manga, browse user curated lists or find recommendations created by our recommendations engine. " />
         <link rel="canonical" href="https://randomanga.net" />
         <link rel="icon" type="image/png" sizes="32x32" href="/logo192.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
