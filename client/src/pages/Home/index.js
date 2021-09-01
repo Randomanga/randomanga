528db8f7e8c31d6f4e14e56fecd01541d4fb90da
Wiz1991 <wizoftime92@gmail.com>
2021-09-01 16:27:50 +0200

    remove helmet from home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -8,19 +8,6 @@ import { Helmet } from 'react-helmet';
 export const Home = (props) => {
   return (
     <Box>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Randomanga - Randomanga</title>
-        <meta property="og:title" content="Randomanga - Randomanga" />
-        <meta name="keywords" content="random manga, random manga generator, random list generator, manga recommendations, list recommendations, manga list, random manga list, manga, manga to read, what manga to read" />
-        <meta property="og:description" content="Need a random manga? Or need some recommendations? Use this website to get a random list of manga, browse user curated lists or find recommendations created by our recommendations engine. " />
-        <link rel="canonical" href="https://randomanga.net" />
-        <meta name="author" content="Wiz" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://randomanga.net" />
-        <meta property="og:image" content="https://randomanga.net/logo512.png" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
-      </Helmet>
       <Daily />
       <FeatureDisplay />
       <GeneratorForm />
